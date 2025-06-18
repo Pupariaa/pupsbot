7084fe0dc04092c027ba3e2acb2ab7e87799a214
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-06-18 21:43:54 +0200

    delete waiting message
    For performance reasons, I cannot maintain this waiting message

--- commands/bm.js
+++ commands/bm.js
@@ -6,7 +6,6 @@ module.exports = {
         child = fork((__dirname, '..', 'workers/bm.js'));
         let user = await getUser(event.nick);
         try {
-            user.locale == 'FR' ? await queue.addToQueue(event.nick, `Attends je réfléchis..`) : await queue.addToQueue(event.nick, `Give me time to think.`);
             child.send({ event, user });
         } catch (e) {
             console.error(e)

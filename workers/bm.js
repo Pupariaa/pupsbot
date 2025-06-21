da8d8e45b6dd3ce03b070c57422ed38cec8d112d
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-06-21 12:36:22 +0200

    wtf bro the console.log()
    Delete useless console.log()

--- workers/bm.js
+++ workers/bm.js
@@ -80,13 +80,9 @@ process.on('message', async (data) => {
         const { min, max } = await computeRefinedGlobalPPRange(data.user.pp, top100Set.tr, data.event.ids, sum);
         results = await findScoresByPPRange({ min, max }, params.mods, data);
 
-        console.log(sum)
-        console.log(sum)
         filtered = filterByMods(results, params.mods, params.allowOtherMods);
 
-        console.log({ min, max })
         const targetPP = computeTargetPP(top100Set.tr, sum);
-        console.log(targetPP)
 
         filtered = filterOutTop100(filtered, top100Set.table);
         filtered = filtered

c55da231363714a549d85cd9bd795c40883f2021
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-06-22 22:57:59 +0200

    Clarified !bm command usage with "nm" mod option
    • Updated help message to mention that "nm" can be used to get only no-mod maps
    • Applied change to both FR and EN versions of the inline command summary
    • Now users can finally stop asking "how do I get nomod maps?" 😩

--- commands/help.js
+++ commands/help.js
@@ -9,8 +9,11 @@ module.exports = {
         const db = new Thread2Database();
         await db.connect();
         let u = await getUser(event.nick)
-        const responseMessage = u.locale === 'FR' ? `Commandes disponible: !bm[Donne une beatmap non jouée(ou jouée il y a longtemps / pas dans ton top rank) mais jouée par quelqu'un ton rang] <mods> | !info[Informations du bot] /np [Donne les pp gains de la map ranked envoyée] | !help [Aide] | !support [Supporter le projet] | !release [Informations sur la mise à jour]`
-            : `Orders available: !bm [Give a beatmap not played (or played a long time ago/not in your top rank) but played by someone your rank] <mods> | !info[Bot information] /np [Give the pp earnings of the ranked map sent] | !help [Help] | !support [Support the project] | !release [Update information]`
+        const responseMessage = u.locale === 'FR'
+            ? `Commandes disponibles: !bm [Donne une beatmap non jouée (ou jouée il y a longtemps / pas dans ton top rank) mais jouée par quelqu'un de ton rang] <mods> (ex: HDHR ou nm pour uniquement des maps sans mods) | !info [Informations du bot] /np [Donne les pp gains de la map ranked envoyée] | !help [Aide] | !support [Supporter le projet] | !release [Informations sur la mise à jour]`
+            : `Orders available: !bm [Give a beatmap not played (or played a long time ago / not in your top rank) but played by someone your rank] <mods> (e.g. HDHR or nm to get only no-mod maps) | !info [Bot information] /np [Give the pp earnings of the ranked map sent] | !help [Help] | !support [Support the project] | !release [Update information]`;
+
+
         await queue.addToQueue(event.nick, responseMessage);
         await db.setHistory(event.id, event.message, responseMessage, u.id, event.nick, true, 0, u.locale);
         await db.disconnect();

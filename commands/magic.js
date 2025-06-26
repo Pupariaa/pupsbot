0dc817cf7c42f897581a3c9123f67e7b4e7e79ca
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-06-26 19:14:56 +0200

    Improve a magic command :)
    Like AI

---
+++ commands/magic.js
@@ -0,0 +1,25 @@
+const { getUser } = require('../services/OsuApiV1');
+const fork = require('child_process').fork;
+const Performe = require('../services/Performe');
+const Logger = require('../utils/Logger');
+
+module.exports = {
+    name: 'magic',
+    async execute(event, args, queue) {
+        const performe = new Performe();
+        try {
+            await performe.markPending(event.id);
+            const child = fork((__dirname, '..', 'workers/magic.js'));
+            const user = await getUser(event.nick);
+
+            child.send({ event, user });
+
+            child.on('message', async (msgFromWorker) => {
+
+            });
+        } catch (e) {
+            Logger.errorCatch('bm', e);
+            await queue.addToQueue(event.nick, "An error occurred while executing the bm command.", false, event.id, false);
+        }
+    }
+};

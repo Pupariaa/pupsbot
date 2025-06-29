d869cf513fb429d59adc22571d44f8f99a085ce4
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-06-29 04:18:09 +0200

    add command !teams

--- commands/help.js
+++ commands/help.js
@@ -15,8 +15,8 @@ module.exports = {
 
             const u = await getUser(event.nick);
             const responseMessage = u.locale === 'FR'
-                ? `Commandes disponibles: !bm [Donne une beatmap non jouée (ou jouée il y a longtemps / pas dans ton top rank) mais jouée par quelqu'un de ton rang] <mods> (ex: HDHR ou nm pour uniquement des maps sans mods) | !info [Informations du bot] /np [Donne les pp gains de la map ranked envoyée] | !help [Aide] | !support [Supporter le projet] | !release [Informations sur la mise à jour] | !fb <feedback> pour donner ton avis constructif sur Pupsbot`
-                : `Orders available: !bm [Give a beatmap not played (or played a long time ago / not in your top rank) but played by someone your rank] <mods> (e.g. HDHR or nm to get only no-mod maps) | !info [Bot information] /np [Give the pp earnings of the ranked map sent] | !help [Help] | !support [Support the project] | !release [Update information] | !fb <feedback> to give constructive feedback on Pupsbot`;
+                ? `Commandes disponibles: !bm [Donne une beatmap non jouée (ou jouée il y a longtemps / pas dans ton top rank) mais jouée par quelqu'un de ton rang] <mods> (ex: HDHR ou nm pour uniquement des maps sans mods) | !info [Informations du bot] /np [Donne les pp gains de la map ranked envoyée] | !help [Aide] | !support [Supporter le projet] | !release [Informations sur la mise à jour] | !fb <feedback> pour donner ton avis constructif sur Pupsbot | !teams [Rejoindre la team Pupsbot]`
+                : `Orders available: !bm [Give a beatmap not played (or played a long time ago / not in your top rank) but played by someone your rank] <mods> (e.g. HDHR or nm to get only no-mod maps) | !info [Bot information] /np [Give the pp earnings of the ranked map sent] | !help [Help] | !support [Support the project] | !release [Update information] | !fb <feedback> to give constructive feedback on Pupsbot | !teams [Join the Pupsbot team]`;
 
             await queue.addToQueue(event.nick, responseMessage, false, event.id, true);
             await db.setHistory(event.id, event.message, responseMessage, u.id, event.nick, true, 0, u.locale);

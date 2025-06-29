5c0e5f11d0e9df365a60baf2e7a14226f4a521b3
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-06-29 16:11:22 +0200

    Extend SuggestedBeatmap model with PP fields and event tracking
    Added 3 new fields to the SuggestedBeatmap Sequelize model:
    
    pp_target (FLOAT): target PP for the suggestion
    
    event_id (BIGINT): link to the associated event
    
    pp_earled (FLOAT): actual PP earned after playing
    
    These fields allow for:
    
    Post-performance evaluation
    
    Event grouping
    
    Comparison between expected and actual results

--- models/SuggestedBeatmap.js
+++ models/SuggestedBeatmap.js
@@ -5,6 +5,9 @@ module.exports = (sequelizeInstance) => {
         user_id: DataTypes.BIGINT,
         beatmap_id: DataTypes.BIGINT,
         Date: DataTypes.DATE,
+        pp_target: DataTypes.FLOAT,
+        event_id: DataTypes.BIGINT,
+        pp_earled: DataTypes.FLOAT
     }, {
         timestamps: false
     });

0fe04fdf3760b7b06ddb72c4316e6ed192ea6a21
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-09-21 03:19:06 +0200

    Add metrics lifecycle to support command
    Execution
    -Initialize collector, create entry, mark pending, connect DB
    -Mark success/error; close DB and metrics in finally
    
    Consistency
    -Aligns support command with new observability standard
    -No functional changes to response text

--- commands/support.js
+++ commands/support.js
@@ -2,14 +2,18 @@ const { getUser } = require('../services/OsuApiV1');
 const Thread2Database = require('../services/SQL');
 const RedisStore = require('../services/RedisStore');
 const Logger = require('../utils/Logger');
+const MetricsCollector = require('../services/MetricsCollector');
 
 module.exports = {
     name: 'support',
     async execute(event, args, queue) {
         const db = new Thread2Database();
         const performe = new RedisStore();
+        const metricsCollector = new MetricsCollector();
 
         try {
+            await metricsCollector.init();
+            await metricsCollector.createCommandEntry(event.id, 'support');
             await db.connect();
             await performe.markPending(event.id);
 
@@ -20,12 +24,15 @@ module.exports = {
 
             await queue.addToQueue(event.nick, responseMessage, false, event.id, true);
             await db.saveCommandHistory(event.id, event.message, responseMessage, u.id, event.nick, true, 0, u.locale);
+            await metricsCollector.updateCommandResult(event.id, 'success');
         } catch (err) {
             Logger.errorCatch('Command::support', err);
+            await metricsCollector.updateCommandResult(event.id, 'error');
             await queue.addToQueue(event.nick, "An error occurred while executing the support command.", false, event.id, false);
         } finally {
             try {
                 await db.disconnect();
+                await metricsCollector.close();
             } catch (e) {
                 Logger.errorCatch('Command::support::disconnect', e);
             }

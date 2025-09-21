a7eea324c3ed06fb38927806878269dc2c9fa727
Maxime <32194766+Pupariaa@users.noreply.github.com>
2025-09-21 03:19:32 +0200

    Add metrics lifecycle to teams command
    Execution
    -Initialize collector and DB; create command entry
    -Record success/error; ensure proper cleanup in finally
    
    Parity
    -Brings teams in line with other instrumented commands
    -No change to command output

--- commands/teams.js
+++ commands/teams.js
@@ -2,14 +2,18 @@ const { getUser } = require('../services/OsuApiV1');
 const Thread2Database = require('../services/SQL');
 const RedisStore = require('../services/RedisStore');
 const Logger = require('../utils/Logger');
+const MetricsCollector = require('../services/MetricsCollector');
 
 module.exports = {
     name: 'teams',
     async execute(event, args, queue) {
         const performe = new RedisStore();
         const db = new Thread2Database();
+        const metricsCollector = new MetricsCollector();
 
         try {
+            await metricsCollector.init();
+            await metricsCollector.createCommandEntry(event.id, 'teams');
             await performe.markPending(event.id);
             await db.connect();
 
@@ -20,12 +24,15 @@ module.exports = {
 
             await queue.addToQueue(event.nick, responseMessage, false, event.id, true);
             await db.saveCommandHistory(event.id, event.message, responseMessage, u.id, event.nick, true, 0, u.locale);
+            await metricsCollector.updateCommandResult(event.id, 'success');
         } catch (err) {
             Logger.errorCatch('Command::teams', err);
+            await metricsCollector.updateCommandResult(event.id, 'error');
             await queue.addToQueue(event.nick, "An error occurred while executing the teams command.", false, event.id, false);
         } finally {
             try {
                 await db.disconnect();
+                await metricsCollector.close();
             } catch (e) {
                 Logger.errorCatch('Command::teams::disconnect', e);
             }
